b665888525b0c0dd976cf96a2390de1f71fb1cbb
KaloyanEnev <128579742+KaloyanEnev@users.noreply.github.com>
2024-08-06 00:23:40 +0300

    add page routes

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # games-play-may-2024
+
 softuni-react-project
 
-
 ## 1. Initialize Project
- - [x] Initializa git repo
- - [x] Add softuni-practice server
- - [x] Add base vite reat project as client
- - [x] CleanUp client
- - [x] Add resources from project resources
- - [x] Convert html to jsx
- - [] Separeta html into components
+
+- [x] Initializa git repo
+- [x] Add softuni-practice server
+- [x] Add base vite reat project as client
+- [x] CleanUp client
+- [x] Add resources from project resources
+- [x] Convert html to jsx
+- [] Separeta html into components
 
 ## 2. Basic Components
- * Install react-router-dom
- * SEtup react-router-dom
- * Add router in App.jsx
- * Add sth
-## 
+
+- [x] Install react-router-dom
+- [x] SEtup react-router-dom
+- [x] Add router in App.jsx
+- [] Add links in the navigation
+
+##
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,31 @@
-import Header from "./components/header/Header"
-
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import GameList from "./components/game-list/GameList";
+import GameCreate from "./components/game-create/GameCreate";
 
 function App() {
-
   return (
     <>
-    <Header/>
-    <main id="main-content">
-      
-        </main>
+      <Header />
+      <main id="main-content">
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/games" element={<GameList />} />
+        <Route path="/games/create" element={<GameCreate />} />
+
+
+
+
+          
+        </Routes>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
